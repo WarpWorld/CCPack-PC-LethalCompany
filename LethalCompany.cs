63ad5c9c52a0b2030c5e9b01dd90ac6ac53ff547
TheUnknownCod3r <114740383+TheUnknownCod3r@users.noreply.github.com>
2024-09-02 04:01:51 +0100

    Add Correct Monster Names
    Fix Monster Names, so they match Scan Nodes instead of Internal Names.

--- LethalCompany.cs
+++ LethalCompany.cs
@@ -52,15 +52,15 @@ namespace CrowdControl.Games.Packs.LethalCompany
                 //new("Night Vision", "nightvision") { Category = "Player", Duration = 30},
                 new("Revive Dead Players", "revive") { Category = "Player"},
 
-                new("Spawn Centipede", "spawn_pede") { Category = "Spawn Enemies"},
+                new("Spawn Snare Flea", "spawn_pede") { Category = "Spawn Enemies"},
                 new("Spawn Nutcracker", "spawn_cracker") { Category = "Spawn Enemies"},
                 new("Spawn Bunker Spider", "spawn_spider") { Category = "Spawn Enemies"},
                 new("Spawn Hoarding Bug", "spawn_hoard") { Category = "Spawn Enemies"},
                 new("Spawn Bracken", "spawn_flower") { Category = "Spawn Enemies"},
-                new("Spawn Crawler", "spawn_crawl") { Category = "Spawn Enemies"},
+                new("Spawn Thumper", "spawn_crawl") { Category = "Spawn Enemies"},
                 new("Spawn Blob", "spawn_blob") { Category = "Spawn Enemies"},
                 new("Spawn Coil-Head", "spawn_spring") { Category = "Spawn Enemies"},
-                new("Spawn Puffer", "spawn_puff") { Category = "Spawn Enemies"},
+                new("Spawn Spore Lizard", "spawn_puff") { Category = "Spawn Enemies"},
                 new("Spawn Eyeless Dog", "spawn_dog") { Category = "Spawn Enemies"},
                 new("Spawn Forest Giant", "spawn_giant") { Category = "Spawn Enemies"},
                 new("Spawn Earth Leviathan", "spawn_levi") { Category = "Spawn Enemies"},
@@ -69,28 +69,28 @@ namespace CrowdControl.Games.Packs.LethalCompany
                 //new("Spawn Cobwebs", "webs") { Category = "Spawn Enemies"},
                 new("Spawn Mimic", "spawn_mimic") { Category = "Spawn Enemies"},
                 new("Spawn Landmine", "spawn_landmine") { Category = "Spawn Enemies"},
-                new("Spawn Old Bird", "spawn_radmech") { Category = "Spawn Enemies"},
-                new("Spawn Clay Surgeon", "spawn_clay") { Category = "Spawn Enemies"},//Tested on Dine
+                new("Spawn Old Bird", "spawn_radmech") { Category = "Spawn Enemies"},//radmech in files, name is Old Bird
+                new("Spawn Barber", "spawn_clay") { Category = "Spawn Enemies"},//Tested on Dine, name is Barber, but is Clay Surgeon in files
                 new("Spawn Butler", "spawn_butler"){ Category = "Spawn Enemies"},//Tested on Dine
 
-                new("Spawn Centipede at Crewmate", "cspawn_pede") { Category = "Spawn Enemies"},
+                new("Spawn Snare Flea at Crewmate", "cspawn_pede") { Category = "Spawn Enemies"},//Snare Flea, Centipede in Files
                 new("Spawn Nutcracker at Crewmate", "cspawn_cracker") { Category = "Spawn Enemies"},
-                new("Spawn Bunker Spider at Crewmate", "cspawn_spider") { Category = "Spawn Enemies"},
+                new("Spawn Bunker Spider at Crewmate", "cspawn_spider") { Category = "Spawn Enemies"},//Sand Spider in files, Bunker Spider in name
                 new("Spawn Hoarding Bug at Crewmate", "cspawn_hoard") { Category = "Spawn Enemies"},
-                new("Spawn Bracken at Crewmate", "cspawn_flower") { Category = "Spawn Enemies"},
-                new("Spawn Crawler at Crewmate", "cspawn_crawl") { Category = "Spawn Enemies"},
+                new("Spawn Bracken at Crewmate", "cspawn_flower") { Category = "Spawn Enemies"},//Flowerman in files, Bracken name. FlowerSnake for Tulip Snake, so a clash here
+                new("Spawn Thumper at Crewmate", "cspawn_crawl") { Category = "Spawn Enemies"},//Thumper, Crawler in Files
                 new("Spawn Blob at Crewmate", "cspawn_blob") { Category = "Spawn Enemies"},
-                new("Spawn Coil-Head at Crewmate", "cspawn_spring") { Category = "Spawn Enemies"},
-                new("Spawn Puffer at Crewmate", "cspawn_puff") { Category = "Spawn Enemies"},
-                new("Spawn Eyeless Dog at Crewmate", "cspawn_dog") { Category = "Spawn Enemies"},
+                new("Spawn Coil-Head at Crewmate", "cspawn_spring") { Category = "Spawn Enemies"},//SpringMan in files
+                new("Spawn Spore Lizard at Crewmate", "cspawn_puff") { Category = "Spawn Enemies"},//Spore Lizard, Puffer in Files
+                new("Spawn Eyeless Dog at Crewmate", "cspawn_dog") { Category = "Spawn Enemies"},//Mouthdog in files
                 new("Spawn Forest Giant at Crewmate", "cspawn_giant") { Category = "Spawn Enemies"},
                 new("Spawn Earth Leviathan at Crewmate", "cspawn_levi") { Category = "Spawn Enemies"},
                 new("Spawn Baboon Hawk at Crewmate", "cspawn_hawk") { Category = "Spawn Enemies"},
                 new("Spawn Ghost Girl at Crewmate", "cspawn_girl") { Category = "Spawn Enemies"},
-                new("Spawn Mimic at Crewmate", "cspawn_mimic") { Category = "Spawn Enemies"},
+                new("Spawn Mimic at Crewmate", "cspawn_mimic") { Category = "Spawn Enemies"},//Mimic, Masked in files
                 new("Spawn Landmine at Crewmate", "cspawn_landmine") { Category = "Spawn Enemies"},
-                new("Spawn Old Bird at Crewmate", "cspawn_radmech") { Category = "Spawn Enemies" },
-                new("Spawn Clay Surgeon at Crewmate", "cspawn_clay") { Category = "Spawn Enemies"},//Tested on Dine
+                new("Spawn Old Bird at Crewmate", "cspawn_radmech") { Category = "Spawn Enemies" },//Old Bird, In files its a radmech
+                new("Spawn Barber at Crewmate", "cspawn_clay") { Category = "Spawn Enemies"},//Tested on Dine, Clay Surgeon In Files, Barber Scannode
                 new("Spawn Butler at Crewmate", "cspawn_butler") { Category = "Spawn Enemies"},//Tested on Dine
 
                 new("Give Binoculars", "give_binoculars") { Category = "Items"},
